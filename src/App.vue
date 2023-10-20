67da7ea454172ad0d35d4f0cbb1a4b6531c6d716
actions.win <actions.win@foxmail.com>
2023-10-20 11:27:47 +0800

    feat: 添加中文配置

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { ElConfigProvider } from 'element-plus'
+</script>
 
-<template>hello world!</template>
+<template>
+  <!-- 项目里面配置了这个中文的就不用倒入这个了 -->
+  <ElConfigProvider :locale="zhCn">Hello world!</ElConfigProvider>
+</template>
